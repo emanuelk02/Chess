5aae991ad1b828c51235ac9d4b74bb73a9e51abc
Emanuel Kupke <emanuel.kupke02@gmail.com>
2021-10-22 12:31:32 +0200

    string tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+object main {
+  @main def hello: Unit =
+    println(oname)
+  println(mesh())
 
-def msg = "I was compiled by Scala 3. :)"
+  def oname = "Welcome to a new game of chess"
+  val eol = sys.props("line.separator")
+  def bar(cellWidth: Int = 3, cellNum: Int = 8) =
+    ("+" + "-" * cellWidth) * cellNum + "+" + eol
+  def cells(cellWidth: Int = 3, cellNum: Int = 8) =
+    ("|" + " " * cellWidth) * cellNum + "|" + eol
+  def mesh(cellWidth: Int = 3, cellNum: Int = 8) =
+    (bar(cellWidth, cellNum)) + cells(cellWidth, cellNum) * cellNum + bar(
+      cellWidth,
+      cellNum
+    )
+
+}
