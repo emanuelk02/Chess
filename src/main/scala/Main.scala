efff70a6e9549c594d7d419f48a543bbb9569cf5
Emanuel Kupke <emanuel.kupke02@gmail.com>
2021-10-22 12:51:53 +0200

    cell bar method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,8 @@
- @main def hello: Unit = {
+object chess {
+  @main def hello: Unit = {
     println(oname)
-  println(mesh())
- }
+    println(mesh())
+  }
 
   def oname = "Welcome to a new game of chess"
   val eol = sys.props("line.separator")
@@ -10,7 +11,8 @@
   def cells(cellWidth: Int = 3, cellNum: Int = 8) =
     ("|" + " " * cellWidth) * cellNum + "|" + eol
   def mesh(cellWidth: Int = 3, cellNum: Int = 8) =
-    (bar(cellWidth, cellNum)) + cells(cellWidth, cellNum) * cellNum + bar(
+    (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(
       cellWidth,
       cellNum
     )
+}
