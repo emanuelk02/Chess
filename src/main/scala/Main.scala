2b1a1f2f2facf011a7d9e01932a925cf36dbcdab
biselli-mar <ma741bis@htwg-konstanz.de>
2021-11-04 20:18:34 +0100

    repl main method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,44 @@
-object chess {
-  @main def hello: Unit = {
-    println(oname)
-    println(mesh())
-  }
+import model.ChessBoard
+import model.Piece
+import model.Matrix
+import model.ChessBoard
+import Piece._
+import Matrix._
+import ChessBoard._
+import scala.io.StdIn.readLine
+import model.Repl
+import Repl._
 
-  def oname = "Welcome to a new game of chess"
-  val eol = sys.props("line.separator")
-  def bar(cellWidth: Int = 3, cellNum: Int = 8) =
-    ("+" + "-" * cellWidth) * cellNum + "+" + eol
-  def cells(cellWidth: Int = 3, cellNum: Int = 8) =
-    ("|" + " " * cellWidth) * cellNum + "|" + eol
-  def mesh(cellWidth: Int = 3, cellNum: Int = 8) =
-    (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(
-      cellWidth,
-      cellNum
-    )
+@main def main: Unit = {
+    val emptyMatr = new Matrix[Option[Piece]](8, None)
+    var newMatr = repl(readLine(">> "), emptyMatr)
+    while(true) {
+      newMatr = repl(readLine(">> "), newMatr)
+    }
+}
+
+def repl(in: String, matr: Matrix[Option[Piece]]): Matrix[Option[Piece]] = {
+    val width = 3
+    val height = 1
+    print(board(width, height, matr))
+    in match {
+        case "i" => {
+            val newMatr = insertManually(matr)
+            print(board(width, height, newMatr))
+            newMatr
+        }
+        case "m" => {
+            val newMatr = move(matr)
+            print(board(width, height, newMatr))
+            newMatr
+        }
+        case "exit" => {
+            System.exit(0)
+            matr
+        }
+        case _ => {
+            print("Unknown command")
+            matr
+        }
+    }
 }
