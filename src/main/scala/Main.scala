6fc57b6d5bc28631e6364a24c51f04ec44c56fe9
biselli-mar <ma741bis@htwg-konstanz.de>
2021-12-17 14:24:34 +0100

    "Game Logo" in Ascii o.o

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,14 @@
+/*                                                                                      *\
+**     _________   ________ _____ ______                                                **
+**    /  ___/  /  / /  ___//  __//  ___/        2021 Emanuel Kupke & Marcel Biselli     **
+**   /  /  /  /_/  /  /__  \  \  \  \           https://github.com/emanuelk02/Chess     **
+**  /  /__/  __   /  /___ __\  \__\  \                                                  **
+**  \    /__/ /__/______//_____/\    /          Software Engineering | HTWG Constance   **
+**   \__/                        \__/                                                   **
+**                                                                                      **
+\*                                                                                      */
+
+
 import de.htwg.se.chess._
 import scala.io.StdIn.readLine
 import aview.TUI
--- src/main/scala/de/htwg/se/chess/util/Matrix.scala
+++ src/main/scala/de/htwg/se/chess/util/Matrix.scala
@@ -1,3 +1,14 @@
+/*                                                                                      *\
+**     _________   ________ _____ ______                                                **
+**    /  ___/  /  / /  ___//  __//  ___/        2021 Emanuel Kupke & Marcel Biselli     **
+**   /  /  /  /_/  /  /__  \  \  \  \           https://github.com/emanuelk02/Chess     **
+**  /  /__/  __   /  /___ __\  \__\  \                                                  **
+**  \    /__/ /__/______//_____/\    /          Software Engineering | HTWG Constance   **
+**   \__/                        \__/                                                   **
+**                                                                                      **
+\*                                                                                      */
+
+
 package de.htwg.se.chess
 package util
 
