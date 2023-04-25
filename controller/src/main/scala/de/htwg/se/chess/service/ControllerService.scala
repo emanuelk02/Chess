fcbcbe68ba42c8f2e3005fcaaea52e44c198fdc0
biselli-mar <ma741bis@htwg-konstanz.de>
2023-04-26 00:21:58 +0200

    made Controller Service more RESTful

--- controller/src/main/scala/de/htwg/se/chess/service/ControllerService.scala
+++ controller/src/main/scala/de/htwg/se/chess/service/ControllerService.scala
@@ -8,7 +8,6 @@
 **                                                                                      **
 \*                                                                                      */
 
-
 package de.htwg.se.chess
 package service
 
@@ -20,180 +19,230 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.StandardRoute
 import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.client.RequestBuilding._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import com.google.inject.Guice
-import scala.concurrent.{Future,ExecutionContextExecutor,ExecutionContext}
-import scala.util.{Try,Success,Failure}
+import scala.concurrent.{Future, ExecutionContextExecutor, ExecutionContext}
+import scala.util.{Try, Success, Failure}
 import scala.quoted._
+import scala.swing.Reactor
 import spray.json._
 
-import util.Tile
-import util.Piece
-import util.FenParser
-import util.ChainHandler
-import util.services.JsonHandlerService
-import util.services.ChessJsonProtocol._
-import controller.controllerComponent.ControllerInterface
-
+import util.data.Tile
+import util.data.Piece
+import util.data.FenParser
+import util.data.ChessJsonProtocol._
+import util.patterns.ChainHandler
+import util.services.SubscricableService
+import controller.controllerComponent._
 
 case class ControllerService(
-     var bind: Future[ServerBinding],
-     ip: String, port: Int,
-     controller: ControllerInterface)
-    (implicit system: ActorSystem[Any],
-              executionContext: ExecutionContext):
-    println(s"Controller running. Please navigate to http://" + ip + ":" + port)
+    var bind: Future[ServerBinding],
+    ip: String,
+    port: Int,
+    controller: ControllerInterface
+)(implicit system: ActorSystem[Any], executionContext: ExecutionContext)
+    extends SubscricableService
+    with Reactor:
+  println(s"Controller running. Please navigate to http://" + ip + ":" + port)
 
-    val route = pathPrefix("controller") { concat(
-            fieldPath,
-            commandPath
-        )}
-
-    val fieldPath =
-        get {  concat(
-            path("fen") {
-                complete(HttpResponse(OK, entity = controller.fieldToFen))
-            },
-            path("cell") {
-                parameter("tile".as[Tile]) { tile =>
-                    controller.cell(tile) match {
-                        case None => complete(HttpResponse(NotFound))
-                        case Some(t) => complete(HttpResponse(OK, entity = t.toJson.toString))
-                    }
-                }
-            },
-            path("selected") {
-                complete(HttpResponse(OK, entity = controller.selected.toJson.toString))
-            },
-            path("legal-moves") {
-                parameter("tile".as[Tile]) { tile =>
-                    complete(HttpResponse(OK, entity = controller.getLegalMoves(tile).toJson.toString))
-                }
-            },
-            path("king-square") {
-                controller.getKingSquare match {
-                    case None => complete(HttpResponse(NotFound))
-                    case Some(sq) => complete(HttpResponse(OK, entity = sq.toJson.toString))
-                }
-            },
-            path("in-check") {
-                complete(HttpResponse(OK, entity = controller.inCheck.toString))
-            },
-            path("is-playing") {
-                complete(HttpResponse(OK, entity = controller.isPlaying.toString))
-            },
-            path("size") {
-                complete(HttpResponse(OK, entity = controller.size.toString))
-            }
-        )}
-
-    val commandPath = concat(
-        put {
-            concat(
-                path("move") {
-                  parameters("from".as[Tile], "to".as[Tile]) { (from, to) =>
-                    complete {
-                        controller.executeAndNotify(controller.move, (from, to))
-                        HttpResponse(OK, entity = controller.fieldToFen)
-                    }
-                  }
-                },
-                path("cell") {
-                  parameters("tile".as[Tile], "piece".as[String]) { (tile, piece) =>
-                    complete {
-                        controller.executeAndNotify(controller.put, (tile, Piece(piece)))
-                        HttpResponse(OK, entity = controller.fieldToFen)
-                    }
-                  }
-                },
-                path("clear") {
-                  complete {
-                    controller.executeAndNotify(controller.clear, ())
-                    HttpResponse(OK, entity = controller.fieldToFen)
-                  }
-                },
-                path("fen") {
-                  parameter("fen".as[String]) { fen =>
-                    complete {
-                        controller.executeAndNotify(controller.putWithFen, fen)
-                        HttpResponse(OK, entity = controller.fieldToFen)
-                    }
-                  }
-                },
-                path("selected") {
-                    parameter("tile".as[Tile].optional) { tile =>
-                        complete {
-                            controller.executeAndNotify(controller.select, tile)
-                            HttpResponse(OK, entity = controller.fieldToFen)
-                        }
-                    }
-                },
-                path("save") {
-                    complete {
-                        controller.save
-                        HttpResponse(OK)
-                    }
-                },
-                path("load") {
-                  complete {
-                    controller.load
-                    HttpResponse(OK)
-                  }
-                },
-                path("start") {
-                  complete {
-                    controller.start
-                    HttpResponse(OK)
-                  }
-                },
-                path("stop") {
-                  complete {
-                    controller.stop
-                    HttpResponse(OK)
-                  }
-                },
-                path("undo") {
-                  complete {
-                    controller.undo
-                    HttpResponse(OK, entity = controller.fieldToFen)
-                  }
-                },
-                path("redo") {
-                  complete {
-                    controller.redo
-                    HttpResponse(OK, entity = controller.fieldToFen)
-                  }
-                },
-                path("exit") {
-                  complete {
-                    controller.exit
-                    terminate
-                    HttpResponse(OK)
-                  }
-                }
-            )
-        }
+  val route = pathPrefix("controller") {
+    concat(
+      fieldPath,
+      commandPath,
+      subscribeRoute
     )
+  }
 
-    def run: Unit =
-        bind = Http().newServerAt(ip, port).bind(route)
+  val fieldPath =
+    get {
+      concat(
+        path("fen") {
+          complete(HttpResponse(OK, entity = controller.fieldToFen))
+        },
+        path("cells") { concat(
+          pathEnd{
+            parameter("tile".as[Tile]) { tile =>
+              controller.cell(tile) match {
+                case None => complete(HttpResponse(NotFound))
+                case Some(t) =>
+                  complete(HttpResponse(OK, entity = t.toJson.toString))
+              }
+            }
+          },
+          path("king-squares") {
+            controller.getKingSquare match {
+              case None => complete(HttpResponse(NotFound))
+              case Some(sq) =>
+                complete(HttpResponse(OK, entity = sq.toJson.toString))
+            }
+          }
+        )},
+        path("states") {
+          concat(
+            parameter("query".as[String]) { query =>
+              query match
+                case "check" =>
+                  complete(
+                    HttpResponse(OK, entity = controller.inCheck.toString)
+                  )
+                case "playing" =>
+                  complete(
+                    HttpResponse(OK, entity = controller.isPlaying.toString)
+                  )
+                case "size" =>
+                  complete(HttpResponse(OK, entity = controller.size.toString))
+                case "selected" =>
+                  complete(HttpResponse(OK,entity = controller.selected.toJson.toString)
+                  )
+                case _ => complete(HttpResponse(NotFound))
+            }
+          )
+        },
+        path("moves") {
+          parameter("tile".as[Tile]) { tile =>
+            complete(HttpResponse(OK, entity = controller.getLegalMoves(tile).toJson.toString))
+          }
+        },
+        path("saves") {
+          complete {
+            controller.load
+            HttpResponse(OK, entity = controller.fieldToFen)
+          }
+        },
+      )
+    }
 
-    def terminate: Unit =
-        bind
-          .flatMap(_.unbind()) // trigger unbinding from the port
-          .onComplete(_ => system.terminate()) // and shutdown when done
+  val commandPath = concat(
+    put {
+      concat(
+        path("moves") {
+          parameters("from".as[Tile], "to".as[Tile]) { (from, to) =>
+            complete {
+              controller.executeAndNotify(controller.move, (from, to))
+              HttpResponse(OK, entity = controller.fieldToFen)
+            }
+          }
+        },
+        path("cells") {
+          parameter("piece".as[String]) ( piece => { concat(
+            parameters("tile".as[Tile]) { tile =>
+              complete {
+                controller.executeAndNotify(controller.put, (tile, Piece(piece)))
+                HttpResponse(OK, entity = controller.fieldToFen)
+              }
+            },
+            parameters("file".as[String], "rank".as[Int]) { (file, rank) =>
+              Try(Tile(file + rank.toString)) match
+                case Success(tile) =>
+                  complete {
+                    controller.executeAndNotify(controller.put, (tile, Piece(piece)))
+                    HttpResponse(OK, entity = controller.fieldToFen)
+                  }
+                case Failure(e) =>
+                  complete(HttpResponse(BadRequest, entity = e.getMessage))
+            },
+            parameter("clear") { _ =>
+              complete {
+                controller.executeAndNotify(controller.clear, ())
+                HttpResponse(OK, entity = controller.fieldToFen)
+              }
+            }
+          )})
+        },
+        path("fen") {
+          parameter("fen".as[String]) { fen =>
+            complete {
+              controller.executeAndNotify(controller.putWithFen, fen)
+              HttpResponse(OK, entity = controller.fieldToFen)
+            }
+          }
+        },
+        path("states") {
+          parameter("query".as[String]) { query =>
+            query match
+              case "playing" =>
+                parameter("state".as[Boolean]) {
+                  case true =>
+                    complete {
+                      controller.start
+                      HttpResponse(OK)
+                    }
+                  case false =>
+                    complete {
+                      controller.stop
+                      HttpResponse(OK)
+                    }
+                }
+              case "selected" =>
+                parameter("tile".as[Tile].optional) { tile =>
+                  complete {
+                    controller.executeAndNotify(controller.select, tile)
+                    HttpResponse(OK, entity = controller.fieldToFen)
+                  }
+                }
+              case _ => complete(HttpResponse(NotFound))
+          }
+        },
+        path("saves") {
+          complete {
+            controller.save
+            HttpResponse(OK)
+          }
+        },
+        path("undo") {
+          complete {
+            controller.undo
+            HttpResponse(OK, entity = controller.fieldToFen)
+          }
+        },
+        path("redo") {
+          complete {
+            controller.redo
+            HttpResponse(OK, entity = controller.fieldToFen)
+          }
+        },
+        path("exit") {
+          complete {
+            controller.exit
+            notifyOnEvent("exit", "\"\"")
+            terminate
+            HttpResponse(OK)
+          }
+        }
+      )
+    }
+  )
 
+  reactions += {
+    case e: CommandExecuted => notifySubscribers(JsString(controller.fieldToFen).toString)
+    case e: MoveEvent =>  notifySubscribers(JsString(controller.fieldToFen).toString)
+    case e: ErrorEvent => notifyOnError(e.msg)
+    case e: Select     => notifyOnEvent("select", e.tile.toJson.toString)
+    case e: GameEnded =>  notifyOnEvent("game-ended", JsString(e.color.get.toString).toString)
+    case _ => ???
+  }
 
-object ControllerService extends JsonHandlerService:
+  def run: Unit =
+    listenTo(controller)
+    bind = Http().newServerAt(ip, port).bind(route)
 
-    val error500 = 
-        "Something went wrong while trying to compute legal moves"
+  def terminate: Unit =
+    bind
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
 
-    def apply(ip: String, port: Int): ControllerService =
-        implicit val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "LegalityComputerService")
-        implicit val executionContext: ExecutionContext = system.executionContext
-        val injector = Guice.createInjector(ChessModule())
-        val controller = injector.getInstance(classOf[ControllerInterface])
-        ControllerService(Future.never, ip, port, controller)
+object ControllerService:
 
-    def apply(): ControllerService = ControllerService("localhost", 8080)
+  val error500 =
+    "Something went wrong while trying to compute legal moves"
+
+  def apply(ip: String, port: Int): ControllerService =
+    implicit val system: ActorSystem[Any] =
+      ActorSystem(Behaviors.empty, "ControllerService")
+    implicit val executionContext: ExecutionContext = system.executionContext
+    val injector = Guice.createInjector(ChessModule())
+    val controller = injector.getInstance(classOf[ControllerInterface])
+    ControllerService(Future.never, ip, port, controller)
+
+  def apply(): ControllerService = ControllerService("localhost", 8080)
