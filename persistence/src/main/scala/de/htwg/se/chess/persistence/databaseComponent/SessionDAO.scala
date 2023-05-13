50e18999e87aee8107cd78d37c57426e0b661f6a
biselli-mar <ma741bis@htwg-konstanz.de>
2023-05-13 19:01:36 +0200

    extend session to use defined data structures

--- persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/SessionDAO.scala
+++ persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/SessionDAO.scala
@@ -15,17 +15,24 @@ package databaseComponent
 
 import scala.concurrent.Future
 import scala.util.Try
+import akka.http.scaladsl.model.Uri
+
+import util.data.Piece
+import util.data.Matrix
+import util.data.ChessState
 
 
-trait SessionDAO {
-    def createSession(userid: Int, fen: String): Future[Try[String]]
-    def createSession(username: String, fen: String): Future[Try[String]]
+trait SessionDao(databseHost: Uri, databasePort: Int) {
+    def createSession(userid: Int, fen: String): Future[Try[Tuple3[Int, Matrix[Option[Piece]], ChessState]]]
+    def createSession(username: String, fen: String): Future[Try[Tuple3[Int, Matrix[Option[Piece]], ChessState]]]
+    def createSession(userid: Int, matr: Matrix[Option[Piece]], state: ChessState): Future[Try[Tuple3[Int, Matrix[Option[Piece]], ChessState]]]
+    def createSession(username: String, matr: Matrix[Option[Piece]], state: ChessState): Future[Try[Tuple3[Int, Matrix[Option[Piece]], ChessState]]]
 
     def readAllSessionsForUser(userid: Int): Future[Try[Seq[Int]]]
     def readAllSessionsForUser(username: String): Future[Try[Seq[Int]]]
-    def readSession(sessionid: Int): Future[Try[String]]
+    def readSession(sessionid: Int): Future[Try[Tuple2[Matrix[Option[Piece]], ChessState]]]
 
-    def updateSession(sessionid: Int, fen: String): Future[Try[String]]
+    def updateSession(sessionid: Int, fen: String): Future[Try[Tuple2[Matrix[Option[Piece]], ChessState]]]
 
-    def deleteSession(sessionid: Int): Future[Try[String]]
+    def deleteSession(sessionid: Int): Future[Try[Tuple2[Matrix[Option[Piece]], ChessState]]]
 }
--- persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/slickImpl/SessionTable.scala
+++ persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/slickImpl/SessionTable.scala
@@ -16,15 +16,23 @@ package slickImpl
 
 import slick.jdbc.PostgresProfile.api._
 
+import util.data.Piece
+import util.data.Matrix
+import util.data.ChessState
+import util.data.FenParser._
 
-class SessionTable(tag: Tag) extends Table[(Int, Int, String)](tag, "session") {
+
+class SessionTable(tag: Tag) extends Table[(Int, String, Int, Tuple2[Matrix[Option[Piece]], ChessState])](tag, "session") {
     def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def displayName = column[String]("display_name")
     def userId = column[Int]("user_id")
     def sessionFen = column[String]("session_fen", O.Length(91, true))
-    override def * = (id, userId, sessionFen)
+    override def * = (id, displayName, userId, sessionFen)
+        <> (
+            (id, displayName, userId, sessionFen) => (id, displayName, userId, (matrixFromFen(sessionFen), stateFromFen(sessionFen))),
+            (id, displayName, userId, matrState) => Some((id, displayName, userId, matrState(0).toFen + matrState(1).toFen))
+        )
 
     def user = foreignKey("user_fk", userId, TableQuery(UsersTable(_)))
         (targetColumns = _.id, onUpdate = ForeignKeyAction.Cascade, onDelete = ForeignKeyAction.Cascade)
-
-    //12345678/12345678/12345678/12345678/12345678/12345678/12345678/12345678 w KQkq A1 100 50
 }
