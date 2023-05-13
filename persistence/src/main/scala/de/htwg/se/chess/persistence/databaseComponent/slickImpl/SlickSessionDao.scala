7b5ddc875bee00502ec4ea4394ede94898a0132d
biselli-mar <ma741bis@htwg-konstanz.de>
2023-05-13 19:01:59 +0200

    scaffolds for slick implementation

---
+++ persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/slickImpl/SlickSessionDao.scala
@@ -0,0 +1,35 @@
+/*                                                                                      *\
+**     _________  ______________________                                                **
+**    /  ___/  / /  /  ____/  ___/  ___/        2023 Emanuel Kupke & Marcel Biselli     **
+**   /  /  /  /_/  /  /__  \  \  \  \           https://github.com/emanuelk02/Chess     **
+**  /  /__/  __   /  /___ __\  \__\  \                                                  **
+**  \    /__/ /__/______/______/\    /         Software Engineering | HTWG Constance    **
+**   \__/                        \__/                                                   **
+**                                                                                      **
+\*                                                                                      */
+
+
+package de.htwg.se.chess
+package persistence
+package databaseComponent
+package slickImpl
+
+import scala.concurrent.Future
+import scala.util.Try
+
+import util.data.User
+import akka.http.scaladsl.model.Uri
+
+
+case class SlickSessionDao(databseHost: Uri, databasePort: Int) {
+    def createSession(userid: Int, fen: String): Future[Try[String]] = ???
+    def createSession(username: String, fen: String): Future[Try[String]] = ???
+
+    def readAllSessionsForUser(userid: Int): Future[Try[Seq[Int]]] = ???
+    def readAllSessionsForUser(username: String): Future[Try[Seq[Int]]] = ???
+    def readSession(sessionid: Int): Future[Try[String]] = ???
+
+    def updateSession(sessionid: Int, fen: String): Future[Try[String]] = ???
+
+    def deleteSession(sessionid: Int): Future[Try[String]] = ???
+}
---
+++ persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/slickImpl/SlickUserDao.scala
@@ -0,0 +1,37 @@
+/*                                                                                      *\
+**     _________  ______________________                                                **
+**    /  ___/  / /  /  ____/  ___/  ___/        2023 Emanuel Kupke & Marcel Biselli     **
+**   /  /  /  /_/  /  /__  \  \  \  \           https://github.com/emanuelk02/Chess     **
+**  /  /__/  __   /  /___ __\  \__\  \                                                  **
+**  \    /__/ /__/______/______/\    /         Software Engineering | HTWG Constance    **
+**   \__/                        \__/                                                   **
+**                                                                                      **
+\*                                                                                      */
+
+
+package de.htwg.se.chess
+package persistence
+package databaseComponent
+package slickImpl
+
+import scala.concurrent.Future
+import scala.util.Try
+
+import util.data.User
+import akka.http.scaladsl.model.Uri
+
+
+case class SlickUserDao(databseHost: Uri, databasePort: Int) {
+    def createUser(name: String, passHash: String): Future[Try[User]] = ???
+
+    def readUser(id: Int): Future[Try[User]] = ???
+    def readUser(name: String): Future[Try[User]] = ???
+    def verifyPass(id: Int, checkHash: String): Future[Try[Boolean]] = ???
+    def verifyPass(name: String, checkHash: String): Future[Try[Boolean]] = ???
+
+    def updateUser(id: Int, newName: String): Future[Try[User]] = ???
+    def updateUser(name: String, newName: String): Future[Try[User]] = ???
+
+    def deleteUser(id: Int): Future[Try[User]] = ???
+    def deleteUser(name: String): Future[Try[User]] = ???
+}
