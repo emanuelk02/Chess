c2a9f2cc437bd3b1c75c18a2b18e553557235475
biselli-mar <ma741bis@htwg-konstanz.de>
2023-05-15 20:15:58 +0200

    change timestamp in SessionTable back to Date

--- persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/slickImpl/SessionTable.scala
+++ persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/slickImpl/SessionTable.scala
@@ -15,6 +15,9 @@ package databaseComponent
 package slickImpl
 
 import slick.jdbc.PostgresProfile.api._
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.sql.Date
 import java.sql.Timestamp
 
 import util.data.Piece
@@ -22,21 +25,20 @@ import util.data.Matrix
 import util.data.ChessState
 import util.data.FenParser._
 import util.data.GameSession
-import java.time.LocalDateTime
 
 
 class SessionTable(tag: Tag) extends Table[(Int, Int, GameSession)](tag, "session") {
     def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
     def userId = column[Int]("user_id")
     def displayName = column[String]("display_name", O.Default("auto-save_"+Timestamp.valueOf(LocalDateTime.now()).toString()))
-    def createDate  = column[Timestamp]("create", O.Default(Timestamp.valueOf(LocalDateTime.now())))
+    def creationDate  = column[Date]("create", O.Default(Date.valueOf(LocalDate.now())))
     def sessionFen = column[String]("session_fen", O.Length(91, true))
-    override def * = (id, userId, displayName, createDate, sessionFen)
+    override def * = (id, userId, displayName, creationDate, sessionFen)
         <> (
-            (id: Int, userId: Int, displayName: String, createTime: Timestamp, sessionFen: String) => 
-                (id, userId, new GameSession(displayName, createTime, sessionFen)),
+            (id: Int, userId: Int, displayName: String, creationDate: Date, sessionFen: String) => 
+                (id, userId, new GameSession(displayName, creationDate, sessionFen)),
             (id: Int, userId: Int, session: GameSession) =>
-                Some((id, userId, session.displayName, session.time, session.toFen))
+                Some((id, userId, session.displayName, session.date, session.toFen))
         )
 
     def user = foreignKey("user_fk", userId, TableQuery(UserTable(_)))
