5b020c5316d3a000f326b349e91ffcb481ccd8f3
biselli-mar <ma741bis@htwg-konstanz.de>
2023-05-23 14:44:04 +0200

    MongoUser case class

---
+++ persistence/src/main/scala/de/htwg/se/chess/persistence/databaseComponent/mongoImpl/MongoUser.scala
@@ -0,0 +1,22 @@
+/*                                                                                      *\
+**     _________  ______________________                                                **
+**    /  ___/  / /  /  ____/  ___/  ___/        2023 Emanuel Kupke & Marcel Biselli     **
+**   /  /  /  /_/  /  /__  \  \  \  \           https://github.com/emanuelk02/Chess     **
+**  /  /__/  __   /  /___ __\  \__\  \                                                  **
+**  \    /__/ /__/______/______/\    /         Software Engineering | HTWG Constance    **
+**   \__/                        \__/                                                   **
+**                                                                                      **
+\*                                                                                      */
+
+
+package de.htwg.se.chess
+package persistence
+package databaseComponent
+package mongoImpl
+
+import org.mongodb.scala.bson.ObjectId
+
+import util.data.User
+
+
+case class MongoUser(_id: Int, name: String, passHash: String)
