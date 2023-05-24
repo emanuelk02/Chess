afc2539456b9a0c5455131f5d7f9cd4d7866a098
biselli-mar <ma741bis@htwg-konstanz.de>
2023-05-24 02:02:00 +0200

    adjust env vars to be more coherent

--- persistence/src/main/scala/de/htwg/se/chess/PersistenceModule.scala
+++ persistence/src/main/scala/de/htwg/se/chess/PersistenceModule.scala
@@ -21,6 +21,9 @@ import slick.lifted.TableQuery
 
 
 object PersistenceModule:
+  println("===================================")
+  println("PersistenceModule")
+  println("env: " + sys.env)
   val sqliteDbFile = java.io.File("./saves/databases/sqlite/chess-persistence.db")
     if !sqliteDbFile.exists then 
         sqliteDbFile.getParentFile.mkdirs()
@@ -30,8 +33,10 @@ object PersistenceModule:
     then slick.jdbc.SQLiteProfile
     else slick.jdbc.PostgresProfile
         
-  given UserDao = new slickImpl.SlickUserDao
-  given SessionDao = new slickImpl.SlickSessionDao
+  //given UserDao = new slickImpl.SlickUserDao
+  //given SessionDao = new slickImpl.SlickSessionDao
+  given UserDao = new mongoImpl.MongoUserDao
+  given SessionDao = new mongoImpl.MongoSessionDao
   given system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "PersistenceService")
   given executionContext: ExecutionContext = system.executionContext
 
